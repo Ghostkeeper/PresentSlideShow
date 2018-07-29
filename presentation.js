12c5fef5ab59ec00d239737c56981b3f2415689b
Ghostkeeper <rubend@tutanota.com>
2018-07-29 23:23:54 +0200

    Move to version control on Git

---
+++ presentation.js
@@ -0,0 +1,275 @@
+function init() {
+	curSlide = 0;
+	curItem = 0;
+	if(window.location.hash.substring(1) != "") {
+		curSlide = parseInt(window.location.hash.substring(1));
+	}
+	resize();
+	show("slide" + curSlide);
+	show("slide" + curSlide + "item" + curItem);
+	
+	updateTimeDisplay();
+
+	document.onkeyup = key;
+	window.onresize = resize;
+	
+	slideEvent();
+}
+
+function nextItem() {
+	curItem += 1;
+	if(curItem >= numitems[curSlide]) {
+		if(curSlide >= numslides - 1) { //Last slide. Don't go beyond.
+			curItem -= 1; //Undo that.
+			return;
+		}
+		for(var i = 0;i < numitems[curSlide];i += 1) {
+			fadeOut("slide" + curSlide + "item" + i);
+		}
+		fadeOut("slide" + curSlide);
+		curSlide += 1;
+		window.location.hash = curSlide;
+		curItem = 0;
+		resize();
+		fadeIn("slide" + curSlide);
+		slideEvent();
+	}
+	var inEffect = document.getElementById("slide" + curSlide + "item" + curItem).getAttribute("in");
+	if(inEffect == "fade") { //Fade in effect.
+		fadeIn("slide" + curSlide + "item" + curItem);
+	} else if(inEffect.substring(0,5) == "slide") { //Slide in effect.
+		slideIn("slide" + curSlide + "item" + curItem,parseFloat(inEffect.substring(5))); //Parse the direction of the slide.
+	}
+	nextItemEvent();
+}
+
+function nextItemEvent() {
+	eval("if(typeof event" + curSlide + "x" + curItem + " == 'function') event" + curSlide + "x" + curItem + "();");
+}
+
+function slideEvent() {
+	eval("if(typeof event" + curSlide + " == 'function') event" + curSlide + "();");
+}
+
+function nextSlide() {
+	if(curSlide >= numslides - 1) { //Last slide. Don't go beyond.
+		return;
+	}
+	for(var i = 0;i < numitems[curSlide];i += 1) {
+		fadeOut("slide" + curSlide + "item" + i);
+	}
+	fadeOut("slide" + curSlide);
+	curSlide += 1;
+	window.location.hash = curSlide;
+	curItem = 0;
+	resize();
+	fadeIn("slide" + curSlide);
+	fadeIn("slide" + curSlide + "item" + curItem);
+	slideEvent();
+}
+
+function previousSlide() {
+	if(curSlide <= 0) { //First slide. Don't go beyond.
+		return;
+	}
+	for(var i = 0;i < numitems[curSlide];i += 1) {
+		fadeOut("slide" + curSlide + "item" + i);
+	}
+	fadeOut("slide" + curSlide);
+	curSlide -= 1;
+	window.location.hash = curSlide;
+	curItem = 0;
+	resize();
+	fadeIn("slide" + curSlide);
+	fadeIn("slide" + curSlide + "item" + curItem);
+	slideEvent();
+}
+
+function goToSlide(goalSlide) {
+	if(goalSlide <= 0) { //Check bounds.
+		goalSlide = 0;
+	} else if(goalSlide >= numslides) {
+		goalSlide = numslides - 1;
+	}
+	if(curSlide == goalSlide) {
+		return; //Already at this slide.
+	}
+	for(var i = 0;i < numitems[curSlide];i += 1) {
+		fadeOut("slide" + curSlide + "item" + i);
+	}
+	fadeOut("slide" + curSlide);
+	curSlide = goalSlide;
+	window.location.hash = curSlide;
+	curItem = 0;
+	resize();
+	fadeIn("slide" + curSlide);
+	fadeIn("slide" + curSlide + "item" + curItem);
+	slideEvent();
+}
+
+function key(e) {
+	var id = (window.event) ? event.keyCode : e.keyCode;
+	switch(id) {
+		case 32: //Spacebar.
+		case 34: //Right arrow on laser pointer (page down).
+			nextItem();
+			break;
+		case 33: //Left arrow on laser pointer (page up).
+		case 37: //Left arrow.
+			previousSlide();
+			break;
+		case 36: //Home.
+			goToSlide(0); //Go to first slide.
+			break;
+		case 35: //End.
+			goToSlide(numslides - 1); //Go to first slide.
+			break;
+		case 39: //Right arrow.
+			nextSlide();
+			break;
+	}
+}
+
+function resize() {
+	document.body.style.fontSize = "" + (window.innerWidth / 40) + "px";
+	var minusHeight = document.getElementById("header" + curSlide).clientHeight + document.getElementById("footer" + curSlide).clientHeight;
+	var iframes = document.getElementsByTagName("iframe");
+	Array.prototype.forEach.call(iframes,function(ifr,i) {
+		ifr.style.height = (window.innerHeight - minusHeight) + "px";
+	});
+	var contents = document.getElementsByClassName("content");
+	Array.prototype.forEach.call(contents,function(cont,i) {
+		cont.style.height = (window.innerHeight - minusHeight) + "px";
+	});
+}
+
+function getStyle(elementId,property) {
+	var element = document.getElementById(elementId);
+	if(element.currentStyle) { //FF and Chrome
+		return element.currentStyle[property];
+	} else { //IE
+		return document.defaultView.getComputedStyle(element,null).getPropertyValue(property);
+	}
+}
+
+function format(num) {
+	if(num < 10) {
+		return "0" + num;
+	} else {
+		return num.toString();
+	}
+}
+
+function updateTimeDisplay() {
+	var dd = new Date();
+	clocks = document.getElementsByClassName("clock"); //Get all clocks on every slide.
+	for(i = 0,len = clocks.length;i < len;i++) {
+		clocks[i].innerHTML = "" + dd.getHours() + ":" + format(dd.getMinutes()) + ":" + format(dd.getSeconds());
+	}
+	setTimeout(updateTimeDisplay,1000); //Update again after 1 second.
+}
+
+function fadeIn(element) {
+	var elem = document.getElementById(element);
+	var opacity = 0; //Initial opacity.
+	elem.style.visibility = "visible"; //But start rendering.
+	var timer = setInterval(function() {
+		if(opacity >= 1) { //Termination criterium.
+			clearInterval(timer);
+		}
+		elem.style.opacity = opacity;
+		elem.style.filter = "alpha(opacity=" + opacity * 100 + ")"; //Legacy browsers.
+		opacity += (1.1 - opacity) * 0.1; //First transitions fast, then slows down towards maximum opacity.
+		if(opacity > 1) opacity = 1; //Shouldn't cause any trouble, but let's just make sure opacity never goes above 1.
+	},10); //Execute this every 10ms.
+}
+
+function fadeOut(element) {
+	var elem = document.getElementById(element);
+	var opacity = 1; //Initial opacity.
+	var timer = setInterval(function() {
+		if(opacity <= 0) { //Termination criterium.
+			clearInterval(timer);
+			elem.style.visibility = "hidden"; //Makes rendering a bit more efficient and prevents button presses on some browsers.
+		}
+		elem.style.opacity = opacity;
+		elem.style.filter = "alpha(opacity=" + opacity * 100 + ")"; //Legacy browsers.
+		opacity -= (0.1 + opacity) * 0.1; //First transitions fast, then slows down towards zero opacity.
+		if(opacity < 0) opacity = 0; //Shouldn't cause any trouble, but let's just make sure opacity never goes below 0.
+	},10); //Execute this every 10ms.
+}
+
+/**
+ * Immediately shows the element, rather than fading in, but prepares it to be properly faded later.
+ */
+function show(element) {
+	var elem = document.getElementById(element);
+	elem.style.visibility = "visible";
+	elem.style.opacity = 1;
+}
+
+/**
+ * Immediately hides the element, rather than fading out, but prepares it to be properly faded later.
+ */
+function hide(element) {
+	var elem = document.getElementById(element);
+	elem.style.visibility = "hidden";
+	elem.style.opacity = 0;
+}
+
+function slideIn(element,direction) {
+	var elem = document.getElementById(element);
+	var slideDistance = Math.max(window.innerWidth,window.innerHeight);
+	direction *= 2 * Math.PI / 360;
+	var fraction = 1;
+	elem.style.left = "" + slideDistance * Math.cos(direction) + "px";
+	elem.style.top = "" + slideDistance * Math.sin(direction) + "px";
+	elem.style.opacity = 1;
+	elem.style.filter = "alpha(opacity=1)"; //Legacy browsers.
+	elem.style.visibility = "visible"; //Start rendering.
+	var timer = setInterval(function() {
+		if(fraction <= 0) { //Termination criterium.
+			clearInterval(timer);
+		}
+		elem.style.left = "" + slideDistance * fraction * Math.cos(direction) + "px";
+		elem.style.top = "" + slideDistance * fraction * Math.sin(direction) + "px";
+		fraction -= (0.1 + fraction) * 0.1; //First transitions fast, then slows down towards minimum fraction.
+		if(fraction <= 0) fraction = 0; //Don't go past 0.
+	},5); //Execute this every 10ms.
+}
+
+function slideTo(element,tox,toy) {
+	var elem = document.getElementById(element);
+	//Note: Left and Top style elements MUST be in percentages!
+	var startx = parseFloat(elem.style.marginLeft.substring(0, elem.style.marginLeft.length - 1)); //Parse the style as float.
+	var starty = parseFloat(elem.style.marginTop.substring(0, elem.style.marginTop.length - 1));
+	var fraction = 0;
+	var diffx = tox - startx;
+	var diffy = toy - starty;
+	var timer = setInterval(function() {
+		if(fraction >= 1) { //Termination criterium.
+			clearInterval(timer);
+		}
+		elem.style.marginLeft = "" + (startx + fraction * diffx) + "%";
+		elem.style.marginTop = "" + (starty + fraction * diffy) + "%";
+		fraction += (1.1 - fraction) * 0.1; //First transitions fast, then slows down towards maximum fraction.
+		if(fraction >= 1) fraction = 1; //Don't go past 1.
+	},10); //Execute this every 10ms.
+}
+
+function moveTo(element,tox,toy) {
+	var elem = document.getElementById(element);
+	//Note: Left and Top style elements MUST be in percentages!
+	elem.style.marginLeft = "" + tox + "%";
+	elem.style.marginTop = "" + toy + "%";
+}
+
+function highLight(item_id) {
+	var elem = document.getElementById("slide" + curSlide + "item" + item_id);
+	elem.classList.add("highlight");
+}
+
+function unHighLight(item_id) {
+	var elem = document.getElementById("slide" + curSlide + "item" + item_id);
+	elem.classList.remove("highlight");
+}
---
+++ presentation.xsl
@@ -0,0 +1,227 @@
+<?xml version="1.0" ?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="html" doctype-system="about:legacy-compat" encoding="UTF-8" indent="yes" media-type="text/html" />
+
+<xsl:template match="/">
+	<html>
+		<head>
+			<title><xsl:value-of select="presentation/@title" /></title>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Prevent IE from entering compatibility mode. -->
+			<link rel="stylesheet" type="text/css" href="presentation.css" />
+			<script type="text/javascript" src="presentation.js" />
+			<script type="text/javascript">
+				numitems = [];
+				<xsl:for-each select="presentation/category/slide">
+					numitems[<xsl:value-of select="position() - 1" />] = <xsl:value-of select="count(item)" />;
+				</xsl:for-each>
+				numslides = <xsl:value-of select="count(presentation/category/slide)" />
+			</script>
+		</head>
+		<body onClick="nextItem()" onLoad="init()">
+			<xsl:if test="presentation/logo">
+				<div class="logo">
+					<img width="100%">
+						<xsl:attribute name="src"><xsl:value-of select="//presentation/@resources" /><xsl:value-of select="presentation/logo" /></xsl:attribute>
+					</img>
+				</div>
+			</xsl:if>
+			<xsl:for-each select="presentation/category/slide">
+				<xsl:variable name="slidenr"><xsl:value-of select="position() - 1" /></xsl:variable>
+				<xsl:if test="script">
+					<script type="text/javascript">
+						function event<xsl:value-of select="count(preceding::slide)" />() {
+							<xsl:value-of select="script" />
+						}
+					</script>
+				</xsl:if>
+				<div style="position:absolute; left:0px; top:0px; width:100%; height:100%; visibility:hidden; opacity:0; filter:alpha(opacity=0);">
+					<xsl:attribute name="id">slide<xsl:value-of select="$slidenr" /></xsl:attribute>
+					
+					<div class="header">
+						<xsl:attribute name="id">header<xsl:value-of select="$slidenr" /></xsl:attribute>
+						<xsl:if test="@titlefont">
+							<xsl:attribute name="style">font-family: <xsl:value-of select="@titlefont" />;</xsl:attribute>
+						</xsl:if>
+						<xsl:value-of select="@title" />
+					</div>
+					
+					<div class="content" style="height:100%;">
+						<xsl:attribute name="style">
+							height: 100%;
+							<xsl:if test="not(@nonewline)">top: -100px;</xsl:if>
+						</xsl:attribute>
+						<xsl:if test="background">
+							<xsl:attribute name="style">background-image: url('<xsl:value-of select="//presentation/@resources" /><xsl:value-of select="background" />')</xsl:attribute>
+						</xsl:if>
+						
+						<xsl:if test="not(item/video)">
+							<br />
+						</xsl:if>
+						
+						<xsl:choose>
+							<xsl:when test="@vertical">
+								<div class="item" style="visibility:hidden; opacity:0; filter:alpha(opacity=0);">
+									<xsl:attribute name="id">slide<xsl:value-of select="$slidenr" />item0</xsl:attribute>
+									<xsl:attribute name="in">
+										<xsl:choose>
+											<xsl:when test="starts-with(@in,'fade')">fade</xsl:when>
+											<xsl:when test="starts-with(@in,'slide')">slide<xsl:value-of select="substring(@in,6)" /></xsl:when>
+											<xsl:otherwise>fade</xsl:otherwise>
+										</xsl:choose>
+									</xsl:attribute>
+									<xsl:apply-templates select="item[1]" />
+								</div>
+								<table style="width:94%; margin-left:3%; height:100%;">
+									<tr style="height:100%;">
+										<xsl:for-each select="item[position() > 1]">
+											<td class="item" style="visibility:hidden; opacity:0; filter:alpha(opacity=0); height:100%">
+												<xsl:attribute name="id">slide<xsl:value-of select="$slidenr" />item<xsl:value-of select="position()" /></xsl:attribute>
+												<xsl:attribute name="in">
+													<xsl:choose>
+														<xsl:when test="starts-with(@in,'fade')">fade</xsl:when>
+														<xsl:when test="starts-with(@in,'slide')">slide<xsl:value-of select="substring(@in,6)" /></xsl:when>
+														<xsl:otherwise>fade</xsl:otherwise>
+													</xsl:choose>
+												</xsl:attribute>
+												<xsl:choose>
+													<xsl:when test="@width">
+														<xsl:attribute name="width"><xsl:value-of select="number(@width) div (count(../item) - 1)" />%</xsl:attribute>
+													</xsl:when>
+													<xsl:otherwise>
+														<xsl:attribute name="width"><xsl:value-of select="number(100) div (count(../item) - 1)" />%</xsl:attribute>
+													</xsl:otherwise>
+												</xsl:choose>
+												<xsl:apply-templates select="." />
+											</td>
+										</xsl:for-each>
+									</tr>
+								</table>
+							</xsl:when>
+							<xsl:otherwise>
+								<xsl:for-each select="item">
+									<div class="item" style="visibility:hidden; opacity:0; filter:alpha(opacity=0);">
+										<xsl:attribute name="id">slide<xsl:value-of select="$slidenr" />item<xsl:value-of select="position() - 1" /></xsl:attribute>
+										<xsl:attribute name="in">
+											<xsl:choose>
+												<xsl:when test="starts-with(@in,'fade')">fade</xsl:when>
+												<xsl:when test="starts-with(@in,'slide')">slide<xsl:value-of select="substring(@in,6)" /></xsl:when>
+												<xsl:otherwise>fade</xsl:otherwise>
+											</xsl:choose>
+										</xsl:attribute>
+										<xsl:apply-templates select="." />
+									</div>
+								</xsl:for-each>
+							</xsl:otherwise>
+						</xsl:choose>
+					</div>
+					
+					<div class="footer">
+						<xsl:attribute name="id">footer<xsl:value-of select="$slidenr" /></xsl:attribute>
+						<div class="footerleft">
+							<xsl:value-of select="../@name" />
+						</div>
+						<div class="footerright">
+							<xsl:value-of select="$slidenr + 1" /> / <xsl:value-of select="count(/presentation/category/slide)" />
+						</div>
+					</div>
+				</div>
+			</xsl:for-each>
+			<div class="footer" style="z-index: -100;">
+				<div class="footerleft" style="z-index: -100;">&#160;</div>
+				<div class="footerright" style="z-index: -100;">&#160;</div>
+			</div>
+		</body>
+	</html>
+</xsl:template>
+
+<xsl:template match="text">
+	<div class="text">
+		<xsl:attribute name="style">
+			<xsl:if test="@size">
+				font-size: <xsl:value-of select="@size" />;
+			</xsl:if>
+			<xsl:if test="@align">
+				text-align: <xsl:value-of select="@align" />;
+			</xsl:if>
+			<xsl:if test="@margin">
+				padding-left: <xsl:value-of select="@margin" />;
+			</xsl:if>
+		</xsl:attribute>
+		<xsl:copy-of select="." />
+	</div>
+</xsl:template>
+
+<xsl:template match="bullet">
+	<ul>
+		<li>
+			<xsl:attribute name="style">
+				<xsl:if test="@size">
+					font-size: <xsl:value-of select="@size" />;
+				</xsl:if>
+			</xsl:attribute>
+			<xsl:copy-of select="." />
+		</li>
+	</ul>
+</xsl:template>
+
+<xsl:template match="image">
+	<div class="image">
+		<img>
+			<xsl:choose>
+				<xsl:when test="@width">
+					<xsl:attribute name="width"><xsl:value-of select="@width" /></xsl:attribute>
+				</xsl:when>
+				<xsl:otherwise>
+					<xsl:attribute name="width">90%</xsl:attribute>
+				</xsl:otherwise>
+			</xsl:choose>
+			<xsl:if test="@height">
+				<xsl:attribute name="height"><xsl:value-of select="@height" /></xsl:attribute>
+			</xsl:if>
+			<xsl:attribute name="src"><xsl:value-of select="//presentation/@resources" /><xsl:value-of select="." /></xsl:attribute>
+		</img>
+	</div>
+</xsl:template>
+
+<xsl:template match="video">
+	<iframe width="100%" frameborder="0" allowfullscreen="yes">
+		<xsl:attribute name="src"><xsl:value-of select="." /></xsl:attribute>
+	</iframe>
+</xsl:template>
+
+<xsl:template match="videoloop">
+	<div align="center">
+		<video loop="yes" autoplay="yes">
+			<xsl:attribute name="width">
+				<xsl:choose>
+					<xsl:when test="@width">
+						<xsl:value-of select="@width" />
+					</xsl:when>
+					<xsl:otherwise>
+						90%
+					</xsl:otherwise>
+				</xsl:choose>
+			</xsl:attribute>
+			<source type="video/ogg">
+				<xsl:attribute name="src"><xsl:value-of select="//presentation/@resources" /><xsl:value-of select="." /></xsl:attribute>
+			</source>
+			OGG/Theora video codec not supported.
+		</video>
+	</div>
+</xsl:template>
+
+<xsl:template match="script">
+	<script type="text/javascript">
+		function event<xsl:value-of select="count(preceding::slide)" />x<xsl:value-of select="count(../preceding-sibling::item)" />() {
+			<xsl:value-of select="." />
+		}
+	</script>
+</xsl:template>
+
+<xsl:template match="clock">
+	<div class="text">
+		<div class="clock">??:??:??</div>
+	</div>
+</xsl:template>
+
+</xsl:stylesheet>
