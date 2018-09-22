b9f6a3ee2d473c28ba8f309d688cd65367dcf3a4
Ghostkeeper <rubend@tutanota.com>
2018-09-22 23:20:29 +0200

    Update code style

--- presentation.js
+++ presentation.js
@@ -7,12 +7,12 @@ function init() {
 	resize();
 	show("slide" + curSlide);
 	show("slide" + curSlide + "item" + curItem);
-	
+
 	updateTimeDisplay();
 
 	document.onkeyup = key;
 	window.onresize = resize;
-	
+
 	slideEvent();
 }
 
@@ -33,7 +33,7 @@ function nextItem() {
 			fadeIn("slide" + curSlide + "item" + curItem);
 		}
 	} else if(inEffect.substring(0, 5) == "slide") { //Slide in effect.
-		slideIn("slide" + curSlide + "item" + curItem,parseFloat(inEffect.substring(5))); //Parse the direction of the slide.
+		slideIn("slide" + curSlide + "item" + curItem, parseFloat(inEffect.substring(5))); //Parse the direction of the slide.
 	}
 	nextItemEvent();
 }
@@ -50,7 +50,7 @@ function nextSlide() {
 	if(curSlide >= numslides - 1) { //Last slide. Don't go beyond.
 		return;
 	}
-	for(var i = 0;i < numitems[curSlide];i += 1) {
+	for(var i = 0; i < numitems[curSlide]; i += 1) {
 		fadeOut("slide" + curSlide + "item" + i);
 	}
 	document.getElementById("slide" + curSlide).style.zIndex = "1";
@@ -69,7 +69,7 @@ function previousSlide() {
 	if(curSlide <= 0) { //First slide. Don't go beyond.
 		return;
 	}
-	for(var i = 0;i < numitems[curSlide];i += 1) {
+	for(var i = 0; i < numitems[curSlide]; i += 1) {
 		fadeOut("slide" + curSlide + "item" + i);
 	}
 	document.getElementById("slide" + curSlide).style.zIndex = "1";
@@ -93,7 +93,7 @@ function goToSlide(goalSlide) {
 	if(curSlide == goalSlide) {
 		return; //Already at this slide.
 	}
-	for(var i = 0;i < numitems[curSlide];i += 1) {
+	for(var i = 0; i < numitems[curSlide]; i += 1) {
 		fadeOut("slide" + curSlide + "item" + i);
 	}
 	document.getElementById("slide" + curSlide).style.zIndex = "1";
@@ -135,21 +135,21 @@ function resize() {
 	document.body.style.fontSize = "" + (window.innerWidth / 40) + "px";
 	var minusHeight = document.getElementById("header" + curSlide).clientHeight + document.getElementById("footer" + curSlide).clientHeight;
 	var iframes = document.getElementsByTagName("iframe");
-	Array.prototype.forEach.call(iframes,function(ifr,i) {
+	Array.prototype.forEach.call(iframes, function(ifr, i) {
 		ifr.style.height = (window.innerHeight - minusHeight) + "px";
 	});
 	var contents = document.getElementsByClassName("content");
-	Array.prototype.forEach.call(contents,function(cont,i) {
+	Array.prototype.forEach.call(contents, function(cont, i) {
 		cont.style.height = (window.innerHeight - minusHeight) + "px";
 	});
 }
 
-function getStyle(elementId,property) {
+function getStyle(elementId, property) {
 	var element = document.getElementById(elementId);
 	if(element.currentStyle) { //FF and Chrome
 		return element.currentStyle[property];
 	} else { //IE
-		return document.defaultView.getComputedStyle(element,null).getPropertyValue(property);
+		return document.defaultView.getComputedStyle(element, null).getPropertyValue(property);
 	}
 }
 
@@ -164,10 +164,10 @@ function format(num) {
 function updateTimeDisplay() {
 	var dd = new Date();
 	clocks = document.getElementsByClassName("clock"); //Get all clocks on every slide.
-	for(i = 0,len = clocks.length;i < len;i++) {
+	for(i = 0, len = clocks.length; i < len; i++) {
 		clocks[i].innerHTML = "" + dd.getHours() + ":" + format(dd.getMinutes()) + ":" + format(dd.getSeconds());
 	}
-	setTimeout(updateTimeDisplay,1000); //Update again after 1 second.
+	setTimeout(updateTimeDisplay, 1000); //Update again after 1 second.
 }
 
 function fadeIn(element) {
@@ -182,7 +182,7 @@ function fadeIn(element) {
 		elem.style.filter = "alpha(opacity=" + opacity * 100 + ")"; //Legacy browsers.
 		opacity += (1.1 - opacity) * 0.1; //First transitions fast, then slows down towards maximum opacity.
 		if(opacity > 1) opacity = 1; //Shouldn't cause any trouble, but let's just make sure opacity never goes above 1.
-	},10); //Execute this every 10ms.
+	}, 10); //Execute this every 10ms.
 }
 
 function fadeOut(element) {
@@ -197,7 +197,7 @@ function fadeOut(element) {
 		elem.style.filter = "alpha(opacity=" + opacity * 100 + ")"; //Legacy browsers.
 		opacity -= (0.1 + opacity) * 0.1; //First transitions fast, then slows down towards zero opacity.
 		if(opacity < 0) opacity = 0; //Shouldn't cause any trouble, but let's just make sure opacity never goes below 0.
-	},10); //Execute this every 10ms.
+	}, 10); //Execute this every 10ms.
 }
 
 /**
@@ -218,9 +218,9 @@ function hide(element) {
 	elem.style.opacity = 0;
 }
 
-function slideIn(element,direction) {
+function slideIn(element, direction) {
 	var elem = document.getElementById(element);
-	var slideDistance = Math.max(window.innerWidth,window.innerHeight);
+	var slideDistance = Math.max(window.innerWidth, window.innerHeight);
 	direction *= 2 * Math.PI / 360;
 	var fraction = 1;
 	elem.style.left = "" + slideDistance * Math.cos(direction) + "px";
@@ -236,10 +236,10 @@ function slideIn(element,direction) {
 		elem.style.top = "" + slideDistance * fraction * Math.sin(direction) + "px";
 		fraction -= (0.1 + fraction) * 0.1; //First transitions fast, then slows down towards minimum fraction.
 		if(fraction <= 0) fraction = 0; //Don't go past 0.
-	},10); //Execute this every 10ms.
+	}, 10); //Execute this every 10ms.
 }
 
-function slideTo(element,tox,toy) {
+function slideTo(element, tox, toy) {
 	var elem = document.getElementById(element);
 	//Note: Left and Top style elements MUST be in percentages!
 	var startx = parseFloat(elem.style.marginLeft.substring(0, elem.style.marginLeft.length - 1)); //Parse the style as float.
@@ -255,10 +255,10 @@ function slideTo(element,tox,toy) {
 		elem.style.marginTop = "" + (starty + fraction * diffy) + "%";
 		fraction += (1.1 - fraction) * 0.1; //First transitions fast, then slows down towards maximum fraction.
 		if(fraction >= 1) fraction = 1; //Don't go past 1.
-	},10); //Execute this every 10ms.
+	}, 10); //Execute this every 10ms.
 }
 
-function moveTo(element,tox,toy) {
+function moveTo(element, tox, toy) {
 	var elem = document.getElementById(element);
 	//Note: Left and Top style elements MUST be in percentages!
 	elem.style.marginLeft = "" + tox + "%";
